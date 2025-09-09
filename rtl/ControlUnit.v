d64d06f48f15ae1424e91c87e34805fc2661dc27
thezhi <13835313+wzx2263796656@user.noreply.gitee.com>
2025-09-09 15:50:02 +0800

    控制单元完成

--- rtl/ControlUnit.v
+++ rtl/ControlUnit.v
@@ -5,11 +5,12 @@ module ControlUnit(
 
     // 控制信号
     output reg[1:0]    m2reg, //决定写回寄存器文件来源。 0：把ALU的运算结果传回，1：把数据存储器的数据传回 2:把立即数左移后的数据传回 
-    output reg[1:0]    PCsrc,    // 控制程序计数器（PC）的更新来源，通常用于分支或跳转操作。PCsrc=0为不跳转，PCsrc=1为跳转PC=PC+offset, PCsrc=2为跳转PC=result(ALU的运算结果)
+    output reg[1:0]    PCsrc,    // 控制程序计数器（PC）的更新来源，通常用于分支或跳转操作。PCsrc=0为不跳转，PCsrc=1为跳转PC=PC+offset, PCsrc=2为跳转PC=result=immExt + A_data(ALU的运算结果)
     output reg    wmem, //控制存储器的写操作, 0为读，1为写
     output reg[2:0] memc,    //控制写一字节还是两个字节，memc=0为 1字节（无符号），memc=1为 2字节（无符号）, memc=2为1个字(32位，4个字节), memc=3为 1字节（有符号）, memc=4为 2字节（有符号）
-    output reg[2:0]  aluc, //控制 ALU 的操作类型，通常用于选择 ALU 的加法、减法、逻辑运算等操作。0：add, 1：sub, 2：and, 3：or 4:beq 5:ble
-    output reg    alusrc,  // 控制 ALU 的输入来源，通常用于选择 ALU 的操作数。0为寄存器的数（add),1为立即数（addi)
+    output reg[3:0]  aluc, //控制 ALU 的操作类型，通常用于选择 ALU 的加法、减法、逻辑运算等操作。共16种，前10种是计算，后六种是逻辑运算（作为条件实现跳转）
+    output reg    alusrc1,  // 控制 ALU 的操作数A输入来源，通常用于选择 ALU 的操作数。0为寄存器的数（add),1为立即数（addi)
+    output reg    alusrc2,  // 控制 ALU 的操作数B输入来源，通常用于选择 ALU 的操作数。0为寄存器的数（add),1为立即数（addi)
     output reg    wreg,  // 控制寄存器的写操作  1为写回，0为不写回
     output reg    jal,   // 控制跳转指令的跳转类型，通常用于选择跳转指令的类型。1为跳转，0为不跳转
     output reg PCHold     // 控制PC的更新，0为更新，1为不更新
@@ -23,13 +24,27 @@ module ControlUnit(
     assign function3 = op[9:7];
     assign function7 = op[16:10];
     
-
+/* 
+    // 默认情况：
+        m2reg = 2'b00;
+        PCsrc = 2'b00;
+        wmem = 1'b0;
+        memc = 3'b000;
+        aluc = 4'b0000;
+        alusrc1 = 1'b0;
+        alusrc2 = 1'b0;
+        wreg = 1'b1;    // 写入寄存器
+        jal = 1'b0;
+        PCHold = 1'b0;
+ */
 
     always @(*) begin
         case(opcode)
             // R型指令: 包括加法、减法、逻辑运算、移位运算 --> rs1 和 rs2的运算 --> aluc 10种
             7'b0110011: begin
-                alusrc = 1'b0;
+                alusrc1 = 1'b0;
+                alusrc2 = 1'b0;
+                wreg = 1'b1;    // 写入寄存器rd
                 case(function3)  
                     3'b000: if(function7 == 7'b0000000) aluc = 4'b0000; //add
                     else if (function7 == 7'b0100000) aluc = 4'b0001;   //sub
@@ -41,12 +56,21 @@ module ControlUnit(
                     else if (function7 == 7'b0100000) aluc = 4'b0111;   //sra
                     3'b110: aluc = 4'b1000;     //or
                     3'b111: aluc = 4'b1001;     //and
-                endcase         
+                endcase
+
+                m2reg = 2'b00;
+                PCsrc = 2'b00;
+                wmem = 1'b0;
+                memc = 3'b000;
+                jal = 1'b0;
+                PCHold = 1'b0;         
             end 
 
-            // I型指令:立即数的运算 --> alusrc, aluc
+            // I型指令:立即数的运算 --> alusrc2, aluc
             7'b0010011: begin
-                alusrc = 1'b1;
+                alusrc1 = 1'b0;
+                alusrc2 = 1'b1;
+                wreg = 1'b1;    // 写入寄存器rd
                 case(function3)
                     3'b000: aluc = 4'b0000; //addi
                     3'b001: aluc = 4'b0010;     //slli
@@ -57,8 +81,14 @@ module ControlUnit(
                     else if (function7 == 7'b0100000) aluc = 4'b0111;   //srai
                     3'b110: aluc = 4'b1000;     //ori
                     3'b111: aluc = 4'b1001;     //andi
-
                 endcase
+
+                m2reg = 2'b00;
+                PCsrc = 2'b00;
+                wmem = 1'b0;
+                memc = 3'b000;
+                jal = 1'b0;
+                PCHold = 1'b0;
             end
 
             // I型指令: load指令 --> wreg, m2reg, memc
@@ -72,31 +102,56 @@ module ControlUnit(
                     3'b100: memc = 3'b000;   // lbu 无符号
                     3'b101: memc = 3'b001;   // lhu 无符号
                 endcase
+
+                PCsrc = 2'b00;
+                wmem = 1'b0;
+                aluc = 4'b0000;
+                alusrc1 = 1'b0;
+                alusrc2 = 1'b0;
+                jal = 1'b0;
+                PCHold = 1'b0;
             end
 
-            // I型指令: jair --> jal,wreg,alusrc,aluc,PCsrc
+            // I型指令: jair --> jal,wreg,alusrc2,aluc,PCsrc
             7'b1100111: begin
                 jal = 1'b1; //实现返回地址保存到rd
-                wreg = 1'b1;
-                alusrc = 1'b1;
-                aluc = 3'b000;
+                wreg = 1'b1;  // 写入寄存器
+                alusrc1 = 1'b0;
+                alusrc2 = 1'b1;
+                aluc = 4'b0000;
                 PCsrc = 2'b10;  //跳转到rs1 + immExt
+
+                m2reg = 2'b00;
+                wmem = 1'b0;
+                memc = 3'b000;
+                PCHold = 1'b0;
             end
 
             // S型指令: store指令 --> wmem, memc
             7'b0100011: begin
                 wmem = 1'b1;
+                wreg = 1'b0;    // 不写入寄存器
                 case(function3)
                     3'b000: memc = 3'b000;   // sb
                     3'b001: memc = 3'b001;   // sh
                     3'b010: memc = 3'b010;   // sw
                 endcase
+
+                m2reg = 2'b00;
+                PCsrc = 2'b00;
+                aluc = 4'b0000;
+                alusrc1 = 1'b0;
+                alusrc2 = 1'b0;
+                jal = 1'b0;
+                PCHold = 1'b0;
             end
 
 
-            // B型指令:条件跳转指令 zero--> alusrc, aluc(6种), PCsrc
+            // B型指令:条件跳转指令 zero--> alusrc2, aluc(6种), PCsrc
             7'b1100011: begin
-                alusrc = 1'b0;  // rs1 和 rs2 参与比较运算
+                alusrc1 = 1'b0;
+                alusrc2 = 1'b0;  // rs1 和 rs2 参与比较运算
+                wreg = 1'b0;    // 不写入寄存器
                 case(function3) 
                     3'b000: aluc = 4'b1010;  //beq
                     3'b001: aluc = 4'b1011;  //bne
@@ -105,26 +160,59 @@ module ControlUnit(
                     3'b010: aluc = 4'b1110;  //bltu
                     3'b111: aluc = 4'b1111;  //bgeu
                 endcase
-
                 if(!zero) PCsrc = 2'b01;  //跳转
                 else  PCsrc = 2'b00;  //不跳转
+
+                m2reg = 2'b00;
+                wmem = 1'b0;
+                memc = 3'b000;
+                jal = 1'b0;
+                PCHold = 1'b0;
             end
             
             // U型指令
             7'b0110111: begin   //lui --> m2reg, jal 左移后的立即数写到rd中
                 m2reg = 2'b10;
                 jal = 1'b0;
+                wreg = 1'b1;    // 写入寄存器
+
+                PCsrc = 2'b00;
+                wmem = 1'b0;
+                memc = 3'b000;
+                aluc = 4'b0000;
+                alusrc1 = 1'b0;
+                alusrc2 = 1'b0;
+                PCHold = 1'b0;
             end
-            7'b0010111: begin   //auipc --> m2reg, jal 当前PC值与左移后的立即数相加
-                m2reg = 2'b10;
+
+            7'b0010111: begin   //auipc --> alusrc1, m2reg, jal 当前PC值与左移后的立即数相加
+                m2reg = 2'b00;
+                alusrc1 = 1'b1;
+                alusrc2 = 1'b1;
+                wreg = 1'b1;    // 写入寄存器
+
+                PCsrc = 2'b00;
+                wmem = 1'b0;
+                memc = 3'b000;
+                aluc = 4'b0000;
+                jal = 1'b0;
+                PCHold = 1'b0;
 
             end
 
-            // J型指令：jal --> jal, wreg, alusrc
-            7'b1101111: begin   //jal --> jal, wreg, alusrc, aluc
+            // J型指令：jal --> jal, wreg, alusrc2
+            7'b1101111: begin   //jal --> jal, wreg, alusrc2, aluc
                 jal = 1'b1;     //实现返回地址保存到rd
-                wreg = 1'b1;
+                wreg = 1'b1;    // 写入寄存器
                 PCsrc = 2'b01;  //跳转到当前PC值 + immExt
+
+                m2reg = 2'b00;
+                wmem = 1'b0;
+                memc = 3'b000;
+                aluc = 4'b0000;
+                alusrc1 = 1'b0;
+                alusrc2 = 1'b0;
+                PCHold = 1'b0;
             end
 
 
