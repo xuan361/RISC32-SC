ffb31037dd398a3ec6511ec44c381245d7c80202
thezhi <13835313+wzx2263796656@user.noreply.gitee.com>
2025-09-10 11:36:21 +0800

    指令寄存器和Rom模块创建和更新

--- rtl/InstructionMemory.v
+++ rtl/InstructionMemory.v
@@ -4,13 +4,12 @@
 // A=4 --> ROM[1]
 // 所以，指令地址需要右移两位才能得到对应的内存地址。
 module InstructionMemory(
-    input CLK,
-    input [31:0] A,   //指令地址输入入口
+    input [31:0] instruction,   //指令
     output reg[16:0] op,    //17位操作码
     output reg[4:0] rs1,    //源操作数1
     output reg[4:0] rs2,    //源操作数2
     output reg[4:0] rd,    //目的操作数
-    output reg[31:0] instruction //传递给立即数扩展模块的指令
+    output reg[31:0] instruction_imm //传递给立即数扩展模块的指令
 
 );
     // 操作码中间变量  
@@ -18,23 +17,19 @@ module InstructionMemory(
     reg[2:0] function3;
     reg[6:0] function7;
 
-    reg[31:0] ROM[0:255]; //ROM 用于存储指令数据
-    // initial 
-    // begin
-    //     $readmemb("D:/learn/RISC32-SC/RISC32-SC/sim/test/test1.txt", ROM);  //读取测试文档中的指令
-    // end
+
 
     always @(*)begin
         // 得到输出的op, rd, rs1, rs2, instruction
-        opcode = ROM[A >> 2][6:0];
-        rd = ROM[A >> 2][11:7];
-        function3 = ROM[A >> 2][14:12];
-        rs1 = ROM[A >> 2][19:15];
-        rs2 = ROM[A >> 2][24:20];
-        function7 = ROM[A >> 2][31:25];
+        opcode = instruction[6:0];
+        function3 = instruction[14:12];
+        function7 = instruction[31:25];
 
+        rd = instruction[11:7];
+        rs1 = instruction[19:15];
+        rs2 = instruction[24:20];
         op = {function7,function3,opcode};
-        instruction = ROM[A >> 2];
+        instruction_imm = instruction;
 
     end
 
